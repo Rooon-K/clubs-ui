8e3ffdab435f450d86ef58c5616818ae03cb4418
yeyou <yeyou@qunhemail.com>
2025-08-01 14:45:43 +0800

    init

---
+++ packages/clubs-ui-docs/theme.config.tsx
@@ -0,0 +1,10 @@
+export default {
+  logo: 'Clubs UI',
+  project: {
+    link: 'https://github.com/Rooon-K/clubs-ui',
+  },
+  docsRepositoryBase: 'https://github.com/Rooon-K/clubs-ui-docs',
+  footer: {
+    text: 'Clubs UI Docs',
+  },
+}
